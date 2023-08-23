6e87648e06a05df70d53388f5d4145e651f35c55
Ian Anjos <78967763+IanAguiar-ai@users.noreply.github.com>
2023-08-23 12:26:21 -0300

    Improving probability logic.
    It is now possible to do conditional probabilities, for example 'func[3] | func[1:4]' i.e. f(x = 3) given that f(x = 1 | x = 2 | x = 3 | x = 4).

--- discrete_function/discrete_function.py
+++ discrete_function/discrete_function.py
@@ -1,5 +1,93 @@
 from random import random
 
+class probability:
+    def __init__(self, value):
+        if type(value) != list:
+            value = [value]
+        self.value = value
+
+    def __or__(self, other):
+        return probability(sum(self.value)/sum(other.value))
+
+    def __div__(self, other):
+        if len(self.value) == len(other.value) == 1:
+            return self.value[0]/other.value[0]
+
+    def __repr__(self):
+        if type(self.value[0]) == list:
+            self.value = self.value[0]
+        return str(self.value)
+
+    def __str__(self):
+        if type(self.value[0]) == list:
+            self.value = self.value[0]
+        return str(self.value)
+
+    def __iadd__(self, value):
+        if type(value) == int or type(value) == float:
+            return probability(self.value[0] + value)
+        elif type(value) == probability:
+            return probability(self.value[0] + value.value[0])
+
+    def __add__(self, value):
+        if type(value) == int or type(value) == float:
+            return probability(self.value[0] + value)
+        elif type(value) == probability:
+            return probability(self.value[0] + value.value[0])
+
+    def __isub__(self, value):
+        if type(value) == int or type(value) == float:
+            return probability(self.value[0] - value)
+        elif type(value) == probability:
+            return probability(self.value[0] - value.value[0])
+
+    def __sub__(self, value):
+        if type(value) == int or type(value) == float:
+            return probability(self.value[0] - value)
+        elif type(value) == probability:
+            return probability(self.value[0] - value.value[0])
+
+    def __pow__(self, value):
+        if type(value) == int or type(value) == float:
+            return probability(self.value[0] ** value)
+        elif type(value) == probability:
+            return probability(self.value[0] ** value.value[0])
+
+    def __lt__(self, value):
+        if type(value) == int or type(value) == float:
+            return self.value[0] < value
+        elif type(value) == probability:
+            return self.value[0] < value.value[0]
+
+    def __le__(self, value):
+        if type(value) == int or type(value) == float:
+            return self.value[0] > value
+        elif type(value) == probability:
+            return self.value[0] > value.value[0]
+
+    def __eq__(self, value):
+        if type(value) == int or type(value) == float:
+            return self.value[0] == value
+        elif type(value) == probability:
+            return self.value[0] == value.value[0]
+
+    def __getitem__(self, index):
+        if type(index) == slice:
+            start, stop = index.start, index.stop
+            if index.start == None:
+                start = 0
+            if index.stop == None:
+                stop = (index.start + 1)*2
+            index = [start, stop]
+            return probability(self.value[index[0]: index[1]])
+        if type(index) == tuple or type(index) == list:
+            return probability(self.value[index[0]: index[1]])
+        return probability(self.value[index])
+            
+
+    def append(self, value):
+        self.value.append(value)
+
 class discrete_function:
     def __init__(self, function = None, *args, **keyargs):
         self.args = args
@@ -8,12 +96,20 @@ class discrete_function:
         self.values = None
         self.value = None
         self.value_accumulated = None
+        self.name = function.__name__
 
     def find(self, x:int = 0):
         if type(x) == int:
-            return self.function(x, *self.args, **self.keyargs)
+            if type(self.value) == probability:
+                return self.function(x, *self.args, **self.keyargs)
+            else:
+                return probability(self.function(x, *self.args, **self.keyargs))
         if type(x) == list or type(x) == tuple:
-            return [self.function(i, *self.args, **self.keyargs) for i in range(x[0], x[1] + 1)]
+            if type(self.value) == probability:
+                return [self.function(i, *self.args, **self.keyargs) for i in range(x[0], x[1] + 1)]
+            else:
+                return probability([self.function(i, *self.args, **self.keyargs) for i in range(x[0], x[1] + 1)])
+            
 
     def accumulated(self, inferior_limit = 0, upper_limit = 10):
         self.values = [self.find(i) for i in range(inferior_limit, upper_limit + 1)]
@@ -21,9 +117,14 @@ class discrete_function:
         self.value = 0
         self.value_accumulated = []
         for value in self.values:
-            self.value += value
+            #print(self.value, value, type(self.value), type(value))
+            self.value = value + self.value
             self.value_accumulated.append(self.value)
-        return self.value
+
+        if type(self.value) == probability:
+            return self.value
+        else:
+            return probability(self.value)
 
     def adjust_to_curve(self, name_param = None, curve:list = None, max_iterations = 100, initial_value = 1):
         """
@@ -85,6 +186,9 @@ class discrete_function:
             k.append(n)
         return k[0] if times == 1 else k
 
+    def __str__(self):
+        return f"Function: {self.name}\nAll args: {self.args} {self.keyargs}"
+
     def __getitem__(self, index):
         if type(index) == slice:
             start, stop = index.start, index.stop
@@ -93,7 +197,10 @@ class discrete_function:
             if index.stop == None:
                 stop = (index.start + 1)*2
             index = [start, stop]
-        return self.find(index)
+        if type(self.find(index)) == probability:
+            return self.find(index)
+        else:
+            return probability(self.find(index))
 
 def rms(curve_1:list, curve_2:list):
     """
@@ -103,5 +210,5 @@ def rms(curve_1:list, curve_2:list):
     if len(curve_1) != len(curve_2):
         raise DifferentListSizesError("Different list sizes")
     for a, b in zip(curve_1, curve_2):
-        resp += (a - b)**2
+        resp = (a - b)**2 + resp
     return resp    
