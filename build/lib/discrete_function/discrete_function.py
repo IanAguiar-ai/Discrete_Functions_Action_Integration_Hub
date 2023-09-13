34c946901417c8f6cb73027a58a4ac925698c2ae
Ian Anjos <78967763+IanAguiar-ai@users.noreply.github.com>
2023-09-13 10:07:58 -0300

    Graphic error corrected.

--- build/lib/discrete_function/discrete_function.py
+++ build/lib/discrete_function/discrete_function.py
@@ -405,7 +405,7 @@ def adjust_sample_on(curve, models, x:list = None, initial_value:float = 0.25, m
             if x == None:
                 x = [i for i  in range(len(curve))]
 
-            x_1 = set([i for i in range(x[0], x[-1])])
+            x_1 = set([i for i in range(int(x[0]), int(x[-1]))])
             x_2 = set(x)
             
             plt.plot(sorted(x_1 | x_2), best_temporary_model[0][list(sorted(x_1 | x_2))].value, label = f'Function')
