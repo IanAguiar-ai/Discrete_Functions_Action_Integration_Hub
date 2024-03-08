8787f6c246ac7fce7e9e2a67587a3c77ad2c478d
Ian Anjos <78967763+IanAguiar-ai@users.noreply.github.com>
2024-03-07 22:38:54 -0300

    Version: 1.2.1

--- discrete_function/__init__.py
+++ discrete_function/__init__.py
@@ -1,1 +1,1 @@
-from .discrete_function import discrete_function, Discrete_function, Df, adjust_sample_on 
+from .discrete_function import discrete_function, Discrete_function, Df, adjust_sample_on, rms 
