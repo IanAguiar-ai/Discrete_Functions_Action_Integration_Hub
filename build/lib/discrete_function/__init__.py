f7d30152d955ca2cd5166cd07fc408d09618ef3c
IanA <iannaianjos@gmail.com>
2024-04-21 15:00:32 -0300

    Version: 1.4.3

--- build/lib/discrete_function/__init__.py
+++ build/lib/discrete_function/__init__.py
@@ -1,1 +1,1 @@
-from .discrete_function import discrete_function, Discrete_function, Df, adjust_sample_on, rms, convert, sample, continuous_accumulation, accumulated_sample 
+from .discrete_function import adjust_sample_on, rms, convert, sample, continuous_accumulation, accumulated_sample, discrete_function, Discrete_function, Df 
